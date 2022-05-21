d7509217ea60ff40536175f990e620ce49b44b07
Renan Neves <renanlneves@gmail.com>
2022-05-21 13:10:36 -0300

    Fibonacci com for

---
+++ 2-Python/Fibonacci/sequencia_fibonacci_com_for.py
@@ -0,0 +1,34 @@
+#perguntando um número para o usuario
+pergunta = int(input("Até Que número você gostaria de fazer a sequencia fibonacci?\n"))
+
+while pergunta != 0:
+    #declarando os dois primeiros números e em seguida a soma dos mesmos. 
+    n1 = 0
+    n2 = 1
+    n3 = n1 + n2
+    #printando as variaveis declaradas e na terceira ilustrando uma conta de adição.
+    print("-" * 60)
+    if pergunta == 1:
+        print(0)
+    elif pergunta == 2:
+        print(0)
+        print(1)
+    elif pergunta == 3:
+        print(0)
+        print(1)
+        print("3° 0 + 1 = 1")
+    else:
+        print(f"1° {n1}")
+        print(f"2° {n2}")
+        print(f"3° {n1} + {n2} = {n3}")
+        # Loop em "for" onde o segundo numero da conta anterior passa a ser o primeiro. 
+        # O segundo da conta anterior numero passa a ser o terceiro
+        # e a variavel num3 faz a soma das variaveis num1 e num2.
+        # O loop começa no 3° e vai até o 10°.
+        for i in range(4, pergunta + 1):
+            n1 = n2
+            n2 = n3
+            n3 = n1 + n2
+
+            print(f"{i}° {n2} + {n1} = {n3}")
+    pergunta = int(input("\nDigite um novo número ou digite \"0\" para terminar\n\n"))
