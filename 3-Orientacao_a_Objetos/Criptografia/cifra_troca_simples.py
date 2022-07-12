6fd78ba27d036dd435ef756c4891efcad70701d3
Renan Neves <renanlneves@gmail.com>
2022-07-12 18:50:26 -0300

    finalizado

--- 3-Orientacao_a_Objetos/Criptografia/cifra_troca_simples.py
+++ 3-Orientacao_a_Objetos/Criptografia/cifra_troca_simples.py
@@ -1,5 +1,29 @@
 """essa cifra troca letras por numeros que se parecem com elas."""
 
+def codificador(txt, dic_codigo):
+    frase_codificada = ""
+
+    for letra in txt:
+        if letra in dic_codigo:
+            letra_codificada = dic_codigo[letra]
+            frase_codificada += letra_codificada
+        else:
+            frase_codificada += letra
+    return frase_codificada
+
+def descodificador(txt, dic_codigo):
+    frase_descodificada = ""
+
+    for letra in txt:
+        if letra in dic_codigo:
+            letra_descodificada = dic_codigo[letra]
+            frase_descodificada += letra_descodificada
+        else:
+            frase_descodificada += letra
+    return frase_descodificada
+
+
+
 frase_secreta = input("Digite a frase que você gostaria de criptografar: \n -> ").upper()
 
 letras_numeros = {
@@ -8,14 +32,17 @@ letras_numeros = {
     "S" : "5",
     "I" : "1"
 }
+numeros_letras = {
+    "4" : "A",
+    "3" : "E",
+    "5" : "S",
+    "1" : "I"
+}
 
-frase_codificada = ""
 
-for letra in frase_secreta:
-    if letra in letras_numeros:
-        letra_codificada = letras_numeros[letra]
-        frase_codificada += letra_codificada
-    else:
-        frase_codificada += letra
+codigo = codificador(frase_secreta, letras_numeros)
+print(f"A frase codificada ficou: \n {codigo}")
 
-print(frase_codificada)
+
+original = descodificador(codigo, numeros_letras)
+print(f"A frase descodificada ficou: \n {original}")
