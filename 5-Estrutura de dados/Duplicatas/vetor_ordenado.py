972d2d9ef131baeb0640ecc2ff8343f8d5ff4b16
Renan Neves <renanlneves@gmail.com>
2022-08-15 19:39:41 -0300

    adicionando arquico de vetores ordenados

--- 5-Estrutura de dados/Duplicatas/vetor_ordenado.py
+++ 5-Estrutura de dados/Duplicatas/vetor_ordenado.py
@@ -1,10 +1,10 @@
-class VetorNaoOrdenado:
+class VetorOrdenado:
     def __init__(self, capacidade, repetidos = False):
         self.repetidos = repetidos
         self.capacidade = capacidade
         self.ultima_posicao = -1
         self.valores = self.capacidade*[0] 
-        
+
     def imprime(self):
         if self.ultima_posicao == -1:
             print('O vetor está vazio')
@@ -14,20 +14,35 @@ class VetorNaoOrdenado:
         if self.repetidos == False:
             self.duplicatas(self.valores)
 
-
     def insere(self, valor):
+
         if self.ultima_posicao == self.capacidade - 1:
-            print("Capacidade máxima atingida")
-        else:
-            self.ultima_posicao = self.ultima_posicao + 1
-            self.valores[self.ultima_posicao] = valor
-    
+            print('Capacidade máxima atingida')
+            return
+
+        posicao = 0
+        for i in range(self.ultima_posicao + 1):
+            posicao = i
+            if self.valores[i] > valor:
+                break
+            if i == self.ultima_posicao:
+                posicao = i + 1
+
+        x = self.ultima_posicao
+        #while x > posicao:
+        while x >= posicao:   
+            self.valores[x + 1] = self.valores[x]
+            x = x - 1
+        
+        self.valores[posicao] = valor
+        self.ultima_posicao = self.ultima_posicao + 1
+
     def pesquisa(self, valor):
         for i in range(self.ultima_posicao + 1):
             if self.valores[i] == valor:
                 return i
         return -1
-    
+
     def excluir(self, valor):
         posicao = self.pesquisa(valor)
         if posicao == -1:
@@ -36,7 +51,6 @@ class VetorNaoOrdenado:
             for i in range(posicao, self.ultima_posicao):
                 self.valores[i] = self.valores[i + 1]
             self.ultima_posicao = self.ultima_posicao - 1
-    
 
     def duplicatas(self, valores): 
         contador = 0
@@ -50,33 +64,27 @@ class VetorNaoOrdenado:
             if dic_duplicados[valor] > 1:
                 lista_duplicados.append(valor)
                 contador += 1
-        if len(lista_duplicados) > 0:
+        if len(lista_duplicados) > 0: 
             print(f"Os números: {lista_duplicados[:]} estão repetindo.")
             return lista_duplicados, contador
         return contador
-
-
-        
-        
-
-
+    
 
 # Chamando a Classe
-vetor = VetorNaoOrdenado(8, False)
+vetor = VetorOrdenado(8)
 
 # =====================================
 # ADICIONEM TESTES A PARTIR DAQUI
 # =====================================
 
-vetor.insere(0)
 vetor.insere(1)
-vetor.insere(6)
+vetor.insere(2)
 vetor.insere(3)
-vetor.insere(6)
+vetor.insere(4)
 vetor.insere(2)
 vetor.insere(6)
-vetor.insere(1)
+vetor.insere(7)
+vetor.insere(8)
 
 vetor.imprime()
-
 
